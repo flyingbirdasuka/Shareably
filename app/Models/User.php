a51df3e2aa521681dadbd1d68fbafa2410d5f2b1
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-02-25 21:41:19 +0100

    merge UserSettings into User

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name', 'email', 'password', 'language'
+        'name', 'email', 'password', 'language', 'notification_setting', 'sound_setting'
     ];
 
     /**
