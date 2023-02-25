31d72bcf401b29d01dbe50b416f2735264809e01
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-02-25 16:51:40 +0100

    adding/updating the migration and Model files

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->boolean('is_admin')->default(0);
+            $table->foreignId('language')->default(1);
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
