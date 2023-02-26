d3aa935ab3e5a81231f0ed420f84118b351c0085
Tom <tomshaddock@googlemai.com>
2023-02-26 11:55:15 +0100

    Add in the 'reader' role to the platform

--- app/Providers/JetstreamServiceProvider.php
+++ app/Providers/JetstreamServiceProvider.php
@@ -57,5 +57,9 @@ class JetstreamServiceProvider extends ServiceProvider
             'create',
             'update',
         ])->description('Editor users have the ability to read, create, and update.');
+
+        Jetstream::role('reader', 'Reader', [
+            'read',
+        ])->description('Reader users have the ability to read.');
     }
 }
