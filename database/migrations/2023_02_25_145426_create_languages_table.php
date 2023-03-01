72bc7de759eeb1330f2e69878768364ec7d22ef2
Tom <tomshaddock@googlemai.com>
2023-03-01 18:33:34 +0100

    Add some default languages to the migration file

--- database/migrations/2023_02_25_145426_create_languages_table.php
+++ database/migrations/2023_02_25_145426_create_languages_table.php
@@ -19,6 +19,32 @@ return new class extends Migration
             $table->string('language');
             $table->timestamps();
         });
+
+        // Load some default languages
+        DB::table('languages')->insert(
+            array(
+                'language_code' => 'en',
+                'language' => 'English',
+                'created_at' => now(),
+                'updated_at' => now(),
+            )
+        );
+        DB::table('languages')->insert(
+            array(
+                'language_code' => 'jp',
+                'language' => 'Japanese',
+                'created_at' => now(),
+                'updated_at' => now(),
+            )
+        );
+        DB::table('languages')->insert(
+            array(
+                'language_code' => 'nl',
+                'language' => 'Dutch',
+                'created_at' => now(),
+                'updated_at' => now(),
+            )
+        );
     }
 
     /**
