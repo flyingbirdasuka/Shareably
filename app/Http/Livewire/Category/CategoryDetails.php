a9530d07cc9f6e6b1bdf623f8cfb657d2ecbee12
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-03-03 05:51:40 +0100

    remove button on the users on the category details page

--- app/Http/Livewire/Category/CategoryDetails.php
+++ app/Http/Livewire/Category/CategoryDetails.php
@@ -11,12 +11,19 @@ class CategoryDetails extends Component
     public $category;
     public $practices;
     public $users;
+    public $user_id;
 
     public function mount($id)
     {
         $this->category = Category::find($id);
         $this->practices = $this->category->practices()->get();
         $this->users = $this->category->users()->get();
+    }
+
+    public function delete($user_id)
+    {
+        Category::where('id',$this->category->id)->first()->users()->detach($user_id);
+        return redirect('categories/'.$this->category->id);
     }
 
     public function render()
--- resources/views/livewire/category/category-details.blade.php
+++ resources/views/livewire/category/category-details.blade.php
@@ -6,7 +6,8 @@
     <br><br><br>
 
     @foreach($users as $user)
-        {{ $user->id }} : {{ $user->name }} <br>
+        {{ $user->id }} : {{ $user->name }}
+        <button wire:click.prevent="delete({{$user->id}})" class="border-solid border-2 border-indigo-600 bg-red-600">REMOVE</button><br>
     @endforeach
 
     <livewire:category.add-user :category_id="$category->id" :users="$users">
