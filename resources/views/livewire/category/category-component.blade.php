00943f86602e79821fdf0103a47d248578abb28e
Tom <tomshaddock@googlemai.com>
2023-03-04 17:02:19 +0100

    Fix to stop the entire row from linking to category edit page

--- resources/views/livewire/category/category-component.blade.php
+++ resources/views/livewire/category/category-component.blade.php
@@ -4,8 +4,10 @@
             <livewire:category.category-edit :category="$category" :key="now() . $category->id" :edit_category="$edit_category" >
         @else
             {{ $category->id }}
-            <b>{{ $category->title }}</b>
-            {{ $category->description }}
+            <a href="categories/{{$category->id}}">
+                <b>{{ $category->title }}</b>
+                {{ $category->description }}
+            </a>
             <button wire:click.prevent="$set('edit_category', true)" class="border-solid border-2 border-indigo-600 bg-green-400" :key="now() . $category->id">EDIT</button>
             <button wire:click.prevent="delete()" class="border-solid border-2 border-indigo-600 bg-red-600">REMOVE</button>
         @endif
--- resources/views/livewire/category/category-section.blade.php
+++ resources/views/livewire/category/category-section.blade.php
@@ -4,8 +4,6 @@
     </label>
     <livewire:category.category-add>
     @foreach ($categories as $category)
-        <a href="categories/{{$category->id}}">
-            <livewire:category.category-component :category="$category" :key="now() . $category->id" :editable="$editable">
-        </a>
+        <livewire:category.category-component :category="$category" :key="now() . $category->id" :editable="$editable">
     @endforeach
 </div>
