8619bffc5f40721a1858fa840d8ccf37ff30f116
Tom <tomshaddock@googlemai.com>
2023-03-04 17:06:46 +0100

    Remove the category link from the edit functionality for categories

--- resources/views/livewire/category/category-component.blade.php
+++ resources/views/livewire/category/category-component.blade.php
@@ -4,17 +4,18 @@
             <livewire:category.category-edit :category="$category" :key="now() . $category->id" :edit_category="$edit_category" >
         @else
             {{ $category->id }}
-            <a href="categories/{{$category->id}}">
-                <b>{{ $category->title }}</b>
-                {{ $category->description }}
-            </a>
+            
+            <b>{{ $category->title }}</b>
+            {{ $category->description }}
+
             <button wire:click.prevent="$set('edit_category', true)" class="border-solid border-2 border-indigo-600 bg-green-400" :key="now() . $category->id">EDIT</button>
             <button wire:click.prevent="delete()" class="border-solid border-2 border-indigo-600 bg-red-600">REMOVE</button>
         @endif
     @else
         {{ $category->id }}
-        <b>{{ $category->title }}</b>
-        {{ $category->description }}
+        <a href="categories/{{$category->id}}">
+            <b>{{ $category->title }}</b>
+            {{ $category->description }}
+        </a>
     @endif
-
 </div>
