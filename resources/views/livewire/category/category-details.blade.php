873f43929639f49ea7c9e02cca4457e682b9f28e
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-03-06 19:06:39 +0100

    adding if statement for view

--- resources/views/livewire/category/category-details.blade.php
+++ resources/views/livewire/category/category-details.blade.php
@@ -4,21 +4,23 @@
     @if($is_admin)
         <button wire:click="$emit('openModal', 'category.add-practice', {{ json_encode(['category_id' => $category->id ]) }})">Add Practice</button>
     @endif
-    @foreach($practices as $practice)
-        {{ $practice->id }} : {{ $practice->title }} : {{ $practice->description }} <br>
-        @if($is_admin)
-            <button wire:click="edit_practice({{$practice->id}})" class="border-solid border-2 border-indigo-600 bg-green-400" :key="now() . $practice->id">EDIT</button>
-            <button wire:click.prevent="delete_practice({{$practice->id}})" class="border-solid border-2 border-indigo-600 bg-red-600">REMOVE</button> <br>
-        @endif
-    @endforeach
+    @if($practices)
+        @foreach($practices as $practice)
+            {{ $practice->id }} : {{ $practice->title }} : {{ $practice->description }} <br>
+            @if($is_admin)
+                <button wire:click="edit_practice({{$practice->id}})" class="border-solid border-2 border-indigo-600 bg-green-400" :key="now() . $practice->id">EDIT</button>
+                <button wire:click.prevent="delete_practice({{$practice->id}})" class="border-solid border-2 border-indigo-600 bg-red-600">REMOVE</button> <br>
+            @endif
+        @endforeach
+    @endif    
     <br><br><br>
     @if($is_admin)
-        @foreach($users as $user)
-            {{ $user->id }} : {{ $user->name }}<br>
-
-            <button wire:click.prevent="delete_user({{$user->id}})" class="border-solid border-2 border-indigo-600 bg-red-600">REMOVE</button><br>
-
-        @endforeach
+        @if($users)
+            @foreach($users as $user)
+                {{ $user->id }} : {{ $user->name }}<br>
+                <button wire:click.prevent="delete_user({{$user->id}})" class="border-solid border-2 border-indigo-600 bg-red-600">REMOVE</button><br>
+            @endforeach
+        @endif
         <button wire:click="$emit('openModal', 'category.add-user', {{ json_encode(['users' => $users, 'category_id' => $category->id ]) }})">Add User</button>
     @endif
 </div>
--- resources/views/livewire/practice/practice-details.blade.php
+++ resources/views/livewire/practice/practice-details.blade.php
@@ -2,8 +2,10 @@
     Title : {{ $practice->title }}
     Description : {{ $practice->description }}
     Category :
-    @foreach($categories as $category)
-        {{ $category->id }}
-    @endforeach
+    @if($categories)
+        @foreach($categories as $category)
+            {{ $category->id }}
+        @endforeach
+    @endif
     <iframe src="{{ $this->pdf }}" width="60%" height="600px;">
 </div>
