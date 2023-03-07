8a8e308c892e4d7739103f36b8aea91a6503749d
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-03-07 07:51:35 +0100

    search function for admin

--- app/Http/Livewire/Practice/PracticeSection.php
+++ app/Http/Livewire/Practice/PracticeSection.php
@@ -8,9 +8,10 @@ use App\Models\User;
 
 class PracticeSection extends Component
 {
-    public $practices;
+    public $practices=[];
     public $user;
     public $is_admin;
+    public $search;
 
     protected $listeners = [
         'refreshParent' => '$refresh',
@@ -21,6 +22,22 @@ class PracticeSection extends Component
         $user = auth()->user();
         $this->practices = $user->is_admin ? Practice::all() : User::find($user)->first()->practices()->get();
         $this->is_admin = $user->is_admin;
+    }
+
+    public function updatedSearch()
+    {
+        // dd($this->practices->where('title', 'like', '%'.$this->search.'%'));
+        $this->practices = Practice::search('title', $this->search)->get();
+        // dd($this->practices->);
+        // foreach($practices as $practice){
+        //     foreach($practice->users() as $user){
+        //         dd($user->id);
+        //     }
+            // if($practice->users()->user_id)){
+            //     dd($practice);
+            // };
+        // };
+
     }
 
     public function render()
