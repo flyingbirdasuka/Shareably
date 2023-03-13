a127e477984498fcc696ae4fe2a8b56fcbbe894e
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-03-13 20:12:50 +0100

    adding the favorite button on the users category details page

--- resources/views/livewire/category/category-details.blade.php
+++ resources/views/livewire/category/category-details.blade.php
@@ -10,10 +10,22 @@
     @endif
     @if($practices)
         @foreach($practices as $practice)
-            {{ $practice->id }} : {{ $practice->title }} : {{ $practice->description }} <br>
+            {{ $practice->id }} : {{ $practice->title }} : {{ $practice->description }}
             @if($is_admin)
                 <button wire:click="edit_practice({{$practice->id}})" class="border-solid border-2 border-indigo-600 bg-green-400" :key="now() . $practice->id">EDIT</button>
                 <button wire:click.prevent="delete_practice({{$practice->id}})" class="border-solid border-2 border-indigo-600 bg-red-600">REMOVE</button> <br>
+            @else
+                @if(in_array($practice->id, $user_practices))
+                    <label>
+                        <input wire:model="user_practices" value="{{ $practice->id }}" type="checkbox" style="display:none;"/>
+                        <i class="fa-solid fa-heart" style="color:red;"></i>
+                    </label><br>
+                @else
+                    <label>
+                        <input wire:model="user_practices" value="{{ $practice->id }}" type="checkbox" style="display:none;"/>
+                        <i class="fa-regular fa-heart"></i>
+                    </label><br>
+                @endif
             @endif
         @endforeach
     @endif    
