aa9b45ee38773ef905959230d9b6ebcdf5bdf6e9
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-03-15 18:54:38 +0100

    fix the show users modal text

--- resources/views/livewire/category/show-users.blade.php
+++ resources/views/livewire/category/show-users.blade.php
@@ -1,6 +1,6 @@
 @if($users)
     <div class="flex flex-col">
-        <p class="font-semibold text-gray-800 p-6">{{ __('categorypage.add_practice') }}</p>
+        <p class="font-semibold text-gray-800 p-6">{{ __('categorypage.show_users') }}</p>
         @foreach($users as $user)
             <div class="flex px-6 py-2 bg-gray-50 justify-between border-b items-center">
                     <p class=>{{ $user->name }}</p>
