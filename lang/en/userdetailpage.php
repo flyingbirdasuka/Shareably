0e0598587875aba21e0d3cf0eff8a5cf7678930e
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-03-17 07:34:19 +0100

    adding the personal member page

---
+++ lang/en/userdetailpage.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'description' => 'description',
+    'status' => 'Status',
+    'active' => 'Active',
+    'member_since' => 'Member Since',
+    'teams' => "Teams",
+    'about' => 'About',
+    'name' => 'Name',
+    'email' => 'Email',
+    'category' => 'Category',
+    'favorite' => 'Favorite'
+
+];
