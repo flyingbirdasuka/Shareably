870798072e4e527b4054a1d95e42cb25b4a87810
Tom <tomshaddock@googlemai.com>
2023-04-01 13:18:58 +0200

    Add pagination to the user list and limit to 10 for display

--- app/Http/Livewire/Users/UsersAll.php
+++ app/Http/Livewire/Users/UsersAll.php
@@ -3,10 +3,13 @@
 namespace App\Http\Livewire\Users;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 use App\Models\User;
 
 class UsersAll extends Component
 {
+    use WithPagination;
+
     public $users;
     public $search;
 
@@ -20,8 +23,16 @@ class UsersAll extends Component
         $this->users = User::search('name', $this->search)->orderBy('name')->get();
     }
 
+    // Reset the pagination after search is run
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
     public function render()
     {
-        return view('livewire.users.users-all');
+        return view('livewire.users.users-all', [
+            'users_list' => User::where('name', 'like', '%'.$this->search.'%')->paginate(10),
+        ]);
     }
 }
