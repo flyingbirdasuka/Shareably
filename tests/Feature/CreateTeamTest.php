3d9366ceb2850bd5d71f0437a1e45297c962391c
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-04-12 13:18:08 +0200

    adjust the DeleteTeamTest

--- tests/Feature/CreateTeamTest.php
+++ tests/Feature/CreateTeamTest.php
@@ -13,7 +13,7 @@ class CreateTeamTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_teams_can_be_created(): void
+    public function test_teams_can_be_created_by_admin(): void
     {
         $user = User::create([
             'name' => 'Asuka Method2',
@@ -34,5 +34,25 @@ class CreateTeamTest extends TestCase
                     ->call('createTeam');
         $this->assertCount(1, $user->fresh()->ownedTeams);
         $this->assertEquals('Test Team', $user->fresh()->ownedTeams()->latest('id')->first()->name);
+    }
+    public function test_teams_can_be_created_by_non_admin(): void
+    {
+        $user = User::create([
+            'name' => 'Asuka Method Non Admin',
+            'email' => 'non_admin@non_admin.com',
+            'email_verified_at' => Carbon::now(),
+            'password' => '$2y$10$3jAFcCj6Gkeigpf.UCEzUuA.xXhIIrrxjYK7xtciBI4bXCAp.cI4.',
+            // vLe064h$0PdN
+            'is_admin' => 0,
+            'current_team_id' => 1, // default all user team
+            "created_at" =>  Carbon::now(),
+            "updated_at" => Carbon::now(),
+        ]);
+
+        $this->actingAs($user);
+        Livewire::test(CreateTeamForm::class)
+                    ->set(['state' => ['name' => 'Test Team']])
+                    ->call('createTeam');
+        $this->assertCount(0, $user->fresh()->ownedTeams);
     }
 }
