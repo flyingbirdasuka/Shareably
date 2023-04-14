75466a153223a913455c7fa688c576419a106246
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-04-14 06:57:28 +0200

    for admin do not show the remove button on all user page

--- resources/views/livewire/users/users-all.blade.php
+++ resources/views/livewire/users/users-all.blade.php
@@ -26,9 +26,11 @@
                 <img class="h-10 w-10 rounded-full object-cover" src="{{ $user->profile_photo_url }}" alt="{{ $user->name }}" />
             </x-table-data>
             <x-table-data><b><a href="users/{{$user->id}}">{{ $user->name }}</a></b></x-table-data>
+            @if(!$user->is_admin)
             <x-table-data>
                 <button wire:click.prevent="$emit('openModal', 'users.remove-user', {{ json_encode(['user_id' => $user->id ]) }})" class="px-4 py-2 text-white font-semibold bg-indigo-500 hover:bg-indigo-700 rounded">{{ __('userspage.remove') }}</button>
             </x-table-data>
+            @endif
         </x-table-row>    
         @empty
             <x-table-row>
