c82732dc3b04676e2abf8e9663686de8a90ee35d
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-05-31 20:48:19 +0200

    fixing the category selector to match the selected category number and details page number

--- app/Http/Livewire/Category/CategoryDetails.php
+++ app/Http/Livewire/Category/CategoryDetails.php
@@ -20,7 +20,7 @@ class CategoryDetails extends Component
 
     public function mount($id)
     {
-        $this->category = Category::find($id)->first();
+        $this->category = Category::find($id);
         $this->practices = $this->category->practices()->orderBy('title')->get();
         $this->users = $this->category->users()->orderBy('name')->get();
         $this->is_admin = auth()->user()->is_admin;
