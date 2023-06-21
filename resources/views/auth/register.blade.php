133324b8c0cccfb5a1ab3e75516c6f5f5a275f7c
flyingbirdasuka <ashuka0424@hotmail.co.jp>
2023-06-21 09:42:09 +0900

    remove the already registered button to force people to accept the invitation

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -47,9 +47,9 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                <!-- <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                     {{ __('auth.already_register') }}
-                </a>
+                </a> -->
 
                 <x-button class="ml-4">
                     {{ __('auth.register') }}
